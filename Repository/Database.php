1f059a89462d1a60ebced62ace352cfec3673ca2
heymanolito <manuelgallardoffs@gmail.com>
2022-02-28 12:44:51 +0100

    add empty composer file

--- Repository/Database.php
+++ Repository/Database.php
@@ -31,7 +31,7 @@ class Database
      */
     private function connect()
     {
-        if ($mysqli = new mysqli(ConfigDB::HOST, ConfigDB::USERNAME, ConfigDB::PASSWORD)) {
+        if ($mysqli = new mysqli(ConfigDB::HOST, ConfigDB::USERNAME, ConfigDB::PASSWORDc)) {
             $sql = "CREATE DATABASE IF NOT EXISTS `Libros` DEFAULT CHARACTER SET utf8 COLLATE 
     utf8_spanish_ci;";
             $mysqli->query($sql);
